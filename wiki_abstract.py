6cd22fb7bdbf3dd61a46be7738edcb710be9e43c
Mathieu Lecarme <mathieu@garambrogne.net>
2023-11-29 17:50:54 +0100

    feat: use lxml. Simpler iterator.

--- wiki_abstract.py
+++ wiki_abstract.py
@@ -1,11 +1,11 @@
 #! /usr/bin/env python
 
-import io
 from dataclasses import dataclass
+from pathlib import Path
 
 from typing import Generator
 import zstandard
-import xml.etree.ElementTree as ET
+from lxml import etree as ET
 
 
 @dataclass(init=False)
@@ -15,38 +15,35 @@ class Doc:
     abstract: str
 
 
-def wiki(p) -> Generator[Doc, None, None]:
+def wiki(p: [Path,str]) -> Generator[Doc, None, None]:
     dctx = zstandard.ZstdDecompressor()
-    parser = ET.XMLPullParser(["start", "end"])
-    # help(dctx.read_to_iter)
     with open(p, "rb") as fh:
-        stream_reader = dctx.stream_reader(fh)
-        text_stream = io.TextIOWrapper(stream_reader, encoding="utf-8")
-        bread_crumbs = []
-        doc = Doc()
-        for line in text_stream:
-            parser.feed(line)
-            for event, elem in parser.read_events():
-                if event == "start":
-                    bread_crumbs.append(elem.tag)
-                elif event == "end":
-                    bread_crumbs.pop()
-                    if elem.tag == "doc":
-                        yield doc
-                        doc = Doc()
-                    elif elem.tag == "title":
-                        if elem.text.startswith("Wikipedia: "):
-                            doc.title = elem.text[11:]
-                        else:
-                            doc.title = elem.text
-                    elif elem.tag == "url":
-                        doc.url = elem.text
-                    elif elem.tag == "abstract":
-                        doc.abstract = elem.text
+        for doc in dctx.stream_reader(fh):
+            yield doc
+
+
+def docs(reader) -> Generator[Doc, None, None]:
+    doc = Doc()
+    for event, elem in ET.iterparse(reader):
+        if event == "end":
+            if elem.tag == "doc":
+                yield doc
+                doc = Doc()
+            elif elem.tag == "title":
+                if elem.text.startswith("Wikipedia: "):
+                    doc.title = elem.text[11:]
+                else:
+                    doc.title = elem.text
+            elif elem.tag == "url":
+                doc.url = elem.text
+            elif elem.tag == "abstract":
+                doc.abstract = elem.text
 
 
 if __name__ == "__main__":
     import sys
+    from tqdm import tqdm
 
-    for doc in wiki(sys.argv[1]):
-        print(doc)
+    reader = wiki(sys.argv[1])
+    for doc in tqdm(reader, unit=" docs", delay=30):
+        pass
